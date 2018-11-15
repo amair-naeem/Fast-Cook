a2a6d12ba722425ea9ba0967fa2476a50cdd5d2c
Amair <a.naeem@se15.qmul.ac.uk>
2018-11-15 21:47:34 +0000

    Update mxgraph.js

--- fastcookapp/static/fastcookapp/mxgraph.js
+++ fastcookapp/static/fastcookapp/mxgraph.js
@@ -1,56 +1,3 @@
-/*
-		// Program starts here. Creates a sample graph in the
-		// DOM node with the specified ID. This function is invoked
-		// from the onLoad event handler of the document (see below).
-		function main(container)
-		{
-			// Checks if the browser is supported
-			if (!mxClient.isBrowserSupported())
-			{
-				// Displays an error message if the browser is not supported.
-				mxUtils.error('Browser is not supported!', 200, false);
-			}
-			else
-			{
-				// Disables the built-in context menu
-				mxEvent.disableContextMenu(container);
-				
-				// Creates the graph inside the given container
-				var graph = new mxGraph(container);
-
-				// Enables rubberband selection
-				new mxRubberband(graph);
-				
-				//mxEvent.addListener(container, 'drop', function(evt)){}
-
-				// Gets the default parent for inserting new cells. This
-				// is normally the first child of the root (ie. layer 0).
-				var parent = graph.getDefaultParent();
-								
-				// Adds cells to the model in a single step
-				//graph.getModel().beginUpdate();
-				try
-				{
-					//var v1 = graph.insertVertex(parent, null, 'Hello,', 20, 20, 80, 30);
-					//var v2 = graph.insertVertex(parent, null, 'World!', 200, 150, 80, 30);
-					//var e1 = graph.insertEdge(parent, null, '', v1, v2);
-					//var toolbar = new mxToolbar(tbContainer);
-                	//toolbar.enabled = false;
-                	var v3 = graph.addVertex('circle.png', 120, 160, 'shape=circle;startSize=20;');
-
-			
-
-				}
-				finally
-				{
-					// Updates the display
-					graph.getModel().endUpdate();
-				}
-			}
-		};
-
-*/
-
 function main()
         {
             // Defines an icon for creating new connections in the connection handler.
@@ -107,6 +54,27 @@ function main()
                 var model = new mxGraphModel();
                 var graph = new mxGraph(container, model);
                 graph.dropEnabled = true;
+
+
+                //remove vertex when delete key is pressed
+                var keyHandler = new mxKeyHandler(graph);
+				keyHandler.bindKey(46, function(evt)
+				{
+				  if (graph.isEnabled())
+				  {
+				    graph.removeCells();
+				  }		
+
+				});
+
+				//remove vertex when backspace is pressed
+				keyHandler.bindKey(8, function(evt)
+				{
+					if (graph.isEnabled())
+					{
+						graph.removeCells();
+					}
+				});
                 
                 // Matches DnD inside the graph
                 mxDragSource.prototype.getDropTarget = function(graph, x, y)
@@ -148,7 +116,7 @@ function main()
                 addVertex('/images/cylinder.gif', 40, 40, 'shape=cylinder');
                 addVertex('/images/actor.gif', 30, 40, 'shape=actor');
                 toolbar.addLine();
-                
+
                 /*var button = mxUtils.button('Create toolbar entry from selection', function(evt)
                 {
                     if (!graph.isSelectionEmpty())
@@ -207,3 +175,4 @@ function main()
             var img = toolbar.addMode(null, image, funct);
             mxUtils.makeDraggable(img, graph, funct);
         }
+
